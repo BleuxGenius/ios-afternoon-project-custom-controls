b1a7da00d95d4bda136edafcc542895128a26301
Dani Blackwell <bleuxgenius@gmail.com>
2019-10-10 15:53:57 -0500

    Steps 1-10

---
+++ StarAppCustomControl/StarAppCustomControl/Custom Control.swift
@@ -0,0 +1,16 @@
+//
+//  Custom Control.swift
+//  StarAppCustomControl
+//
+//  Created by Lambda_School_Loaner_167 on 10/10/19.
+//  Copyright © 2019 Dani. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+    let labels: [UILabel] = []
+    
+}
---
+++ StarAppCustomControl/StarAppCustomControl/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  StarAppCustomControl
+//
+//  Created by Lambda_School_Loaner_167 on 10/10/19.
+//  Copyright © 2019 Dani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        var titleString = "User Rating: \(ratingControl.value)"
+        titleString += ratingControl.value == 1 ? "star" : "stars"
+        title = titleString
+    }
+    
+    
+
+
+}
+
