b579f245439d9ad9cfa3124a205496e392e41d0c
Dani Blackwell <bleuxgenius@gmail.com>
2019-10-11 00:44:03 -0500

    completed 1-8 of the custom control portion

--- StarAppCustomControl/StarAppCustomControl/Custom Control.swift
+++ StarAppCustomControl/StarAppCustomControl/Custom Control.swift
@@ -11,15 +11,59 @@ import UIKit
 class CustomControl: UIControl {
     
     var value: Int = 1
-    let labels: [UILabel] = []
+    var labels: [UILabel] = []
     
     private let componentDimension: CGFloat = 40.0
     private let componentCount = 5
-    private let componenetActiveColor = UIColor.black
-    private let compenentInactiveColor = UIColor.gray
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUp()
+       
+    }
     
+//    MARK: - Methods
     
+    func setUp() {
+        
+//         use a loop to create 5 labels
+        for i in 1...componentCount {
+//            creating the label
+            let label = UILabel()
+            label.tag = i
+            addSubview(label)
+//            turn off the autolayout
+            label.translatesAutoresizingMaskIntoConstraints = false
+//            constrain the label
+            label.heightAnchor.constraint(equalToConstant: componentDimension).isActive = true
+            label.widthAnchor.constraint(equalToConstant: componentDimension).isActive = true
+            let offset = (40 * (i - 1)) + (8 * 1)
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: CGFloat(offset)).isActive = true
+//
+            label.font = .boldSystemFont(ofSize: 32)
+            label.text = "★"
+            label.textAlignment = .center
+            label.textColor = i > value ? componentInactiveColor : componentActiveColor
+            labels.append(label)
+            
+        }
+        
+    }
     
+//    informs compiler how how big custom control should be
+    override var intrinsicContentSize: CGSize {
+        
+        let componentWidth = CGFloat(componentCount) * componentDimension
+        let componentSpacing = CGFloat(componentCount + 1) * 8
+        let width = componentWidth + componentSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
     
+    func updateLabels() {
+        for i in 1...componentCount {
+            labels[i-1].textColor = i > value ? componentInactiveColor : componentActiveColor
+        }
+    }
 }
